6fac02b4b22dbb546f3c82d84ace8d22563f84e4
Kelvedler <hnflkmvjcm@gmail.com>
2024-05-10 19:50:27 +0300

    fix keygen

--- migration/000001_initial.up.sql
+++ migration/000001_initial.up.sql
@@ -19,7 +19,7 @@ CREATE TABLE api_user(
   created_at timestamptz NOT NULL DEFAULT now(),
   updated_at timestamptz NOT NULL DEFAULT now(), 
   name varchar(30) NOT NULL,
-  key varchar(128) NOT NULL,
+  secret varchar(128) NOT NULL,
   uses integer NOT NULL DEFAULT 0,
   active boolean DEFAULT TRUE,
   UNIQUE(name)
